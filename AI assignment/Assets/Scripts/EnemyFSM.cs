bb2bda9071fb0bc385bf6f112344fcec6b26a2ce
Aloy <aloyang123@gmail.com>
2024-07-27 15:33:32 -0700

    fixed enemy going back to patrol when reached gun pos

--- AI assignment/Assets/Scripts/EnemyFSM.cs
+++ AI assignment/Assets/Scripts/EnemyFSM.cs
@@ -707,6 +707,13 @@ public class EnemyFSM : MonoBehaviour
     public float shootingRange = 20f;
     public float fireRate = 1f;
 
+    //Ray Casting
+    public GameObject OriginRay;
+    private LineRenderer[] lineRenderers;
+    public float rotationTolerance = 0.1f;  // Tolerance to stop rotating when close enough
+    public float rotationSpeed = 5f;
+    private Vector3[] directions = new Vector3[45];
+
     public GameObject bulletPrefab;
     public Transform bulletPoint;
     public GameObject player;
@@ -723,14 +730,143 @@ public class EnemyFSM : MonoBehaviour
 
     void Start()
     {
+
+        directions[0] = Vector3.forward;  // Center
+        directions[1] = (Vector3.forward + 0.05f * Vector3.up).normalized;  // Top
+        directions[2] = (Vector3.forward + 0.05f * Vector3.down).normalized;  // Bottom
+        directions[3] = (Vector3.forward + 0.5f * Vector3.left).normalized;  // Left
+        directions[4] = (Vector3.forward + 0.5f * Vector3.right).normalized;  // Right
+        directions[5] = (Vector3.forward + 0.05f * Vector3.up + 0.05f * Vector3.left).normalized;  // Top left
+        directions[6] = (Vector3.forward + 0.05f * Vector3.up + 0.05f * Vector3.right).normalized;  // Top right
+        directions[7] = (Vector3.forward + 0.05f * Vector3.down + 0.05f * Vector3.left).normalized;  // Bottom left
+        directions[8] = (Vector3.forward + 0.05f * Vector3.down + 0.05f * Vector3.right).normalized;  // Bottom right
+
+
+        directions[9] = (Vector3.forward + 0.05f * Vector3.up + 0.05f * Vector3.left).normalized;  // Top left
+        directions[10] = (Vector3.forward + 0.05f * Vector3.up + 0.15f * Vector3.left).normalized;  // Top left
+        directions[11] = (Vector3.forward + 0.05f * Vector3.up + 0.25f * Vector3.left).normalized;  // Top left
+        directions[12] = (Vector3.forward + 0.05f * Vector3.up + 0.35f * Vector3.left).normalized;  // Top left
+        directions[13] = (Vector3.forward + 0.05f * Vector3.up + 0.45f * Vector3.left).normalized;  // Top left
+        directions[14] = (Vector3.forward + 0.05f * Vector3.up + 0.55f * Vector3.left).normalized;  // Top left
+
+        directions[15] = (Vector3.forward + 0.05f * Vector3.left).normalized;  // Top left
+        directions[16] = (Vector3.forward + 0.15f * Vector3.left).normalized;  // Top left
+        directions[17] = (Vector3.forward + 0.25f * Vector3.left).normalized;  // Top left
+        directions[18] = (Vector3.forward + 0.35f * Vector3.left).normalized;  // Top left
+        directions[19] = (Vector3.forward + 0.45f * Vector3.left).normalized;  // Top left
+        directions[20] = (Vector3.forward + 0.55f * Vector3.left).normalized;  // Top left
+
+        directions[21] = (Vector3.forward + 0.05f * Vector3.down + 0.05f * Vector3.left).normalized;  // Top left
+        directions[22] = (Vector3.forward + 0.05f * Vector3.down + 0.15f * Vector3.left).normalized;  // Top left
+        directions[23] = (Vector3.forward + 0.05f * Vector3.down + 0.25f * Vector3.left).normalized;  // Top left
+        directions[24] = (Vector3.forward + 0.05f * Vector3.down + 0.35f * Vector3.left).normalized;  // Top left
+        directions[25] = (Vector3.forward + 0.05f * Vector3.down + 0.45f * Vector3.left).normalized;  // Top left
+        directions[26] = (Vector3.forward + 0.05f * Vector3.down + 0.55f * Vector3.left).normalized;  // Top left
+
+
+        directions[27] = (Vector3.forward + 0.05f * Vector3.up + 0.05f * Vector3.right).normalized;  // Top left
+        directions[28] = (Vector3.forward + 0.05f * Vector3.up + 0.15f * Vector3.right).normalized;  // Top left
+        directions[29] = (Vector3.forward + 0.05f * Vector3.up + 0.25f * Vector3.right).normalized;  // Top left
+        directions[30] = (Vector3.forward + 0.05f * Vector3.up + 0.35f * Vector3.right).normalized;  // Top left
+        directions[31] = (Vector3.forward + 0.05f * Vector3.up + 0.45f * Vector3.right).normalized;  // Top left
+        directions[32] = (Vector3.forward + 0.05f * Vector3.up + 0.55f * Vector3.right).normalized;  // Top left
+
+        directions[33] = (Vector3.forward + 0.05f * Vector3.right).normalized;  // Top left
+        directions[34] = (Vector3.forward + 0.15f * Vector3.right).normalized;  // Top left
+        directions[35] = (Vector3.forward + 0.25f * Vector3.right).normalized;  // Top left
+        directions[36] = (Vector3.forward + 0.35f * Vector3.right).normalized;  // Top left
+        directions[37] = (Vector3.forward + 0.45f * Vector3.right).normalized;  // Top left
+        directions[38] = (Vector3.forward + 0.55f * Vector3.right).normalized;  // Top left
+
+        directions[39] = (Vector3.forward + 0.05f * Vector3.down + 0.05f * Vector3.right).normalized;  // Top left
+        directions[40] = (Vector3.forward + 0.05f * Vector3.down + 0.15f * Vector3.right).normalized;  // Top left
+        directions[41] = (Vector3.forward + 0.05f * Vector3.down + 0.25f * Vector3.right).normalized;  // Top left
+        directions[42] = (Vector3.forward + 0.05f * Vector3.down + 0.35f * Vector3.right).normalized;  // Top left
+        directions[43] = (Vector3.forward + 0.05f * Vector3.down + 0.45f * Vector3.right).normalized;  // Top left
+        directions[44] = (Vector3.forward + 0.05f * Vector3.down + 0.55f * Vector3.right).normalized;  // Top left
+
+
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        audioSource = GetComponent<AudioSource>();
+
+        // Create LineRenderer instances
+        lineRenderers = new LineRenderer[45];
+        for (int i = 0; i < lineRenderers.Length; i++)
+        {
+            GameObject lrObject = new GameObject("LineRenderer_" + i);
+            lrObject.transform.SetParent(transform);
+            lineRenderers[i] = lrObject.AddComponent<LineRenderer>();
+            lineRenderers[i].startWidth = 0.05f;
+            lineRenderers[i].endWidth = 0.05f;
+            lineRenderers[i].sortingOrder = 5;  // Adjust as needed
+            lineRenderers[i].material = new Material(Shader.Find("Sprites/Default"));  // Default material for rendering
+        }
+
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         audioSource = GetComponent<AudioSource>();
         currentState = State.Idle;
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
+    void showRay()
+    {
+
+
+        Vector3 closestHitPoint = Vector3.zero;
+
+        for (int i = 0; i < directions.Length; i++)
+        {
+            Vector3 direction = transform.TransformDirection(directions[i]);
+
+            if (Physics.Raycast(OriginRay.transform.position, direction, out RaycastHit hitinfo, detectionRange))
+            {
+                // Check if the hit object is the player
+                if (hitinfo.collider.CompareTag("Player"))
+                {
+                    closestHitPoint = hitinfo.point;
+
+                    // Set the positions for the LineRenderer to visualize the raycast hit
+                    lineRenderers[i].SetPosition(0, OriginRay.transform.position);
+                    lineRenderers[i].SetPosition(1, hitinfo.point);
+
+                    // Set the color of the line to red
+                    lineRenderers[i].startColor = Color.red;
+                    lineRenderers[i].endColor = Color.red;
+
+                    // var playerAttributes = hitinfo.collider.GetComponent<AttributeManager>();
+                    // if (playerAttributes != null)
+                    // {
+                    //     AttributeManager.DealDamage(hitinfo.collider.gameObject);
+                    // }
+                }
+                else
+                {
+                    // Hit something else (e.g., wall), draw to the hit point
+                    lineRenderers[i].SetPosition(0, OriginRay.transform.position);
+                    lineRenderers[i].SetPosition(1, hitinfo.point);
+
+                    // Set the color of the line to yellow
+                    lineRenderers[i].startColor = Color.yellow;
+                    lineRenderers[i].endColor = Color.yellow;
+                }
+            }
+            else
+            {
+                // If no hit, draw the full length ray
+                lineRenderers[i].SetPosition(0, OriginRay.transform.position);
+                lineRenderers[i].SetPosition(1, OriginRay.transform.position + direction * detectionRange);
+
+                // Set the color of the line to green
+                lineRenderers[i].startColor = Color.green;
+                lineRenderers[i].endColor = Color.green;
+            }
+        }
+    }
+
     void Update()
     {
+        showRay();
+
         switch (currentState)
         {
             case State.Idle:
@@ -764,17 +900,52 @@ public class EnemyFSM : MonoBehaviour
         // Calculate the distance to the player
         float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
+        //// Check if the bullet is not null
+        //if (bullet != null)
+        //{
+        //    hasDetectedBullet = true;
+        //    bulletInitialPosition = bullet.transform.position;
+        //}
+
+        //if (hasDetectedBullet)
+        //{
+        //    agent.destination = bulletInitialPosition;
+        //    Debug.Log("Following bullet at: " + bulletInitialPosition);
+        //}
+
+        //// Check if the player is within shooting range
+        //if (distanceToPlayer <= shootingRange)
+        //{
+        //    currentState = State.Shooting;
+        //}
+        //// Check if the player is within detection range and in front of the enemy
+        //else if (distanceToPlayer < detectionRange && angleToPlayer <= maxAngle)
+        //{
+        //    currentState = State.Chasing;
+        //}
+        //else if (currentState == State.Chasing && distanceToPlayer >= detectionRange)
+        //{
+        //    currentState = State.Patrolling;
+        //}
         // Check if the bullet is not null
         if (bullet != null)
         {
             hasDetectedBullet = true;
             bulletInitialPosition = bullet.transform.position;
+            currentState = State.GoLocation; // Transition to GoLocation state when bullet is detected
         }
 
-        if (hasDetectedBullet)
+        if (hasDetectedBullet && currentState == State.GoLocation)
         {
             agent.destination = bulletInitialPosition;
             Debug.Log("Following bullet at: " + bulletInitialPosition);
+
+            // Check if the enemy has reached the bullet's initial position
+            if (Vector3.Distance(transform.position, bulletInitialPosition) < 5f)
+            {
+                hasDetectedBullet = false; // Reset bullet detection
+                currentState = State.Patrolling; // Transition back to patrolling
+            }
         }
 
         // Check if the player is within shooting range
